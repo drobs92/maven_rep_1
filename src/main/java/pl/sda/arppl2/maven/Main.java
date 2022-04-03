0f07055cabbae62d3f4351ac57547c6135212989
Robert Piechowski <robert.t.piechowski@gmail.com>
2022-04-03 13:51:50 +0200

    pierwszy commit

---
+++ src/main/java/pl/sda/arppl2/maven/Main.java
@@ -0,0 +1,10 @@
+package pl.sda.arppl2.maven;
+
+public class Main {
+    //generuję metodę startową psvm (public static void)
+    public static void main(String[] args) {
+//generuje komendę wypisania -sout -> system out print line
+        System.out.println("Hello World");
+
+    }
+}
